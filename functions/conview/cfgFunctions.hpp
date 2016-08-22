e953cb943130355e7200d07f5c2102eb38b41069
McDiod <hobbeldobbel@gmail.com>
2016-08-22 19:10:51 +0200

    removed getFactionClasses from cfgFunctions

--- functions/conview/cfgFunctions.hpp
+++ functions/conview/cfgFunctions.hpp
@@ -3,10 +3,6 @@ class GRAD_conview
 	{
      class conview
 		 {
-			  class getFactionClasses {
-					                        description = "Find all Classnames of given Faction.";
-																	preInit = 1;
-																};
         class getFactions {
 														description = "Find all FactionNames in Config.";
 														preInit = 1;
