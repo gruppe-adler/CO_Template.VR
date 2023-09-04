44bed89d7945beb2508b77340e8806976ee0953b
nomisum <nomisum@googlemail.com>
2023-09-04 23:53:54 +0200

    Update userLoadouts.hpp

--- USER/userLoadouts.hpp
+++ USER/userLoadouts.hpp
@@ -1,4 +1,4 @@
-#include "node_modules\grad-factions\medicloadoutBase.hpp"
+#include "..\node_modules\grad-factions\medicloadoutBase.hpp"
 /*
 *   Hier können eigene factions für grad-loadout eingetragen werden.
 *   Anleitung im Wiki.
